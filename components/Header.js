27e6a54ca7ceacfedbc4fc9ee37a04dccec35897
Akshay Kudalkar <105927588+Akshaykk12@users.noreply.github.com>
2025-02-08 09:46:28 +0530

    Updated Resume

--- components/Header.js
+++ components/Header.js
@@ -35,7 +35,7 @@ const Header = () => {
                     priority= {true}  />
           </Link>
         <Socials/>
-        <a  href="https://drive.google.com/file/d/1LlBLxXsd3z8BCvxM7VDRMbHxBFSbbQ-k/view?usp=sharing" 
+        <a  href="https://drive.google.com/file/d/1b1oxM2xDiuTnVYrUUJmWH21YDmLjZsWp/view?usp=sharing" 
             target='_blank'>
           <button
             className=" xl:ml-4 px-4 py-2 z-0
